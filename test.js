7e3d4ad474ab9110e94ca5722f519e477baa6e83
Rasmus Frederiksen <rasmus@vector.dk>
2019-12-11 12:26:26 +0100

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-let countries = require('./countries');
-const fs = require('fs');
-
-for (let i = 0; i < countries.length; i++) {
-  let c = countries[i];
-  console.log(`${c.names[0]} - ${c.iso2}/${c.iso3} - ${c.population}`);
-}
-
